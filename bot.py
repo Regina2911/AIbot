704ed5e6aa90c5b9f20fefb1974765ab3fc9feda
Rysha <reginalee9@mail.ru>
2025-09-27 12:20:28 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot
+import requests
+bot = telebot.TeleBot('')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(message, "Привет! Я бот для получения и анализа изображений. Отправь фото для анализа изображения")
+
+@bot.message_handler(content_types=['photo'])
+def handle_docs_photo(message):
+    '''При отправке изображения, проводит его анализ'''
+    if not message.photo:
+        return bot.send_message(message.chat.id, "Вы забыли загрузить картинку :(")
+
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = file_info.file_path.split('/')[-1]
+
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+bot.polling()
